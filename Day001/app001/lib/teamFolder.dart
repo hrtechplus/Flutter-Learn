fb0f8d8975e64f14ba67ab0780dc7419038a69fb
HasinduOnline <40927446+hrtechplus@users.noreply.github.com>
2024-06-20 14:14:50 +0530

    Create teamFolder.dart

---
+++ Day001/app001/lib/teamFolder.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+class TeamFolderPage extends StatefulWidget {
+  TeamFolderPage({super.key});
+
+  @override
+  State<TeamFolderPage> createState() => _TeamFolderPageState();
+}
+
+class _TeamFolderPageState extends State<TeamFolderPage> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Scaffold(
+      appBar: AppBar(
+        title: Text('Calculator'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+           ElevatedButton(onPressed: (){ print("clicked")}, child: Text("Click me")),
+           SizedBox(height: 20),
+            Text("Team Folder Page"),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              child: Text("Back to Home"),
+            ),
+          ],
+        ),
+      ),
+    ));
+  }
+}
