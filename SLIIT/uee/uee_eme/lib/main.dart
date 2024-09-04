b79b03681b25f0a1c9605f09efc8b318101f9b5f
HasinduOnline <40927446+hrtechplus@users.noreply.github.com>
2024-09-05 01:16:53 +0530

    Refactor main.dart, add email/password form widget, and update dependencies-sos_screen01

--- SLIIT/uee/uee_eme/lib/main.dart
+++ SLIIT/uee/uee_eme/lib/main.dart
@@ -1,157 +1,164 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
-    home: MainApp(),
-  ));
+  runApp(MyApp());
 }
 
-class MainApp extends StatefulWidget {
-  @override
-  _MainAppState createState() => _MainAppState();
-}
-
-class _MainAppState extends State<MainApp> {
-  int _selectedIndex = 0;
-  final List<Widget> _screens = [
-    HomeScreen(),
-    UserProfileScreen(),
-  ];
-
-  void _onItemTapped(int index) {
-    setState(() {
-      _selectedIndex = index;
-    });
-  }
-
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: IndexedStack(
-        index: _selectedIndex,
-        children: _screens,
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: 'Home',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.person),
-            label: 'Profile',
-          ),
-        ],
-        currentIndex: _selectedIndex,
-        selectedItemColor: Colors.amber[800],
-        onTap: _onItemTapped,
-      ),
+    return MaterialApp(
+      home: EmergencyHomeScreen(),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
 
-class HomeScreen extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Text(
-          "Home Screen",
-          style: TextStyle(
-              fontSize: 24.0, fontWeight: FontWeight.bold), // Explicit styling
-        ),
-      ),
-    );
-  }
-}
-
-class UserProfileScreen extends StatelessWidget {
-  final _formKey = GlobalKey<FormState>();
-
+class EmergencyHomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: LayoutBuilder(builder: (context, constraints) {
-          return SingleChildScrollView(
-            padding: const EdgeInsets.symmetric(horizontal: 16.0),
-            child: Column(
-              children: [
-                SizedBox(height: constraints.maxHeight * 0.08),
-                Image.network(
-                  "https://i.postimg.cc/nz0YBQcH/Logo-light.png",
-                  height: 100,
-                ),
-                SizedBox(height: constraints.maxHeight * 0.08),
-                Text(
-                  "Medical Profile",
-                  style: Theme.of(context)
-                      .textTheme
-                      .headlineSmall!
-                      .copyWith(fontWeight: FontWeight.bold),
-                ),
-                SizedBox(height: constraints.maxHeight * 0.05),
-                Form(
-                  key: _formKey,
-                  child: Column(
+        child: Column(
+          children: [
+            // Custom App Bar
+            Padding(
+              padding:
+                  const EdgeInsets.symmetric(horizontal: 16.0, vertical: 24.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Icon(Icons.menu, size: 30, color: Colors.grey[700]),
+                  Row(
                     children: [
-                      buildTextFormField(context, 'Full Name'),
-                      buildTextFormField(context, 'Blood Group'),
-                      buildTextFormField(context, 'Age'),
-                      buildTextFormField(context, 'Additional Note'),
-                      buildTextFormField(context, 'Emergency Contact'),
-                      Padding(
-                        padding: const EdgeInsets.symmetric(vertical: 16.0),
-                        child: ElevatedButton(
-                          onPressed: () {
-                            if (_formKey.currentState!.validate()) {
-                              _formKey.currentState!.save();
-                              // Process data here or navigate to another screen
-                            }
-                          },
-                          style: ElevatedButton.styleFrom(
-                            elevation: 0,
-                            backgroundColor: const Color(0xFF00BF6D),
-                            foregroundColor: Colors.white,
-                            minimumSize: const Size(double.infinity, 48),
-                            shape: const StadiumBorder(),
-                          ),
-                          child: const Text("Save Profile"),
-                        ),
+                      Text(
+                        "Emergency",
+                        style: TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.grey[700]),
+                      ),
+                      Text(
+                        "App",
+                        style: TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.red),
                       ),
                     ],
                   ),
-                ),
-              ],
+                  CircleAvatar(
+                    backgroundImage: NetworkImage(
+                        'https://i.postimg.cc/3wJPXggT/profile-image.jpg'),
+                    radius: 20,
+                  ),
+                ],
+              ),
             ),
-          );
-        }),
+
+            // SOS Button Section
+            Expanded(
+              child: Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    GestureDetector(
+                      onTap: () {
+                        // Implement SOS logic
+                      },
+                      child: Container(
+                        height: 150,
+                        width: 150,
+                        decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          gradient: LinearGradient(
+                            colors: [Colors.pinkAccent, Colors.red],
+                            begin: Alignment.topCenter,
+                            end: Alignment.bottomCenter,
+                          ),
+                        ),
+                        child: Center(
+                          child: Text(
+                            "SOS",
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 28,
+                                fontWeight: FontWeight.bold),
+                          ),
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 20),
+                    Text(
+                      "Press for 3 seconds",
+                      style: TextStyle(
+                          color: Colors.grey[700],
+                          fontSize: 16,
+                          fontWeight: FontWeight.w500),
+                    ),
+                    SizedBox(height: 10),
+                    Text(
+                      "KEEP CALM!",
+                      style: TextStyle(
+                          color: Colors.red,
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold),
+                    ),
+                    Text(
+                      "After pressing SOS button, you will get help in 10 minutes.",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        color: Colors.grey[600],
+                        fontSize: 14,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+
+            // Custom Bottom Navigation Bar
+            Container(
+              padding: EdgeInsets.symmetric(vertical: 16.0),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black12,
+                    blurRadius: 10,
+                    offset: Offset(0, -2),
+                  ),
+                ],
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  buildNavBarItem(Icons.local_pharmacy, "Pharmacy"),
+                  buildNavBarItem(Icons.local_hospital, "Clinic"),
+                  buildNavBarItem(Icons.phone, "Consultation"),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
 
-  Widget buildTextFormField(BuildContext context, String hint) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(vertical: 8.0),
-      child: TextFormField(
-        decoration: InputDecoration(
-          hintText: hint,
-          filled: true,
-          fillColor: Color(0xFFF5FCF9),
-          contentPadding:
-              EdgeInsets.symmetric(horizontal: 16.0 * 1.5, vertical: 16.0),
-          border: OutlineInputBorder(
-            borderSide: BorderSide.none,
-            borderRadius: BorderRadius.all(Radius.circular(50)),
+  Widget buildNavBarItem(IconData icon, String label) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Icon(icon, color: Colors.grey[700], size: 28),
+        SizedBox(height: 4),
+        Text(
+          label,
+          style: TextStyle(
+            color: Colors.grey[700],
+            fontSize: 12,
           ),
         ),
-        validator: (value) {
-          if (value == null || value.isEmpty) {
-            return 'Please enter $hint';
-          }
-          return null;
-        },
-      ),
+      ],
     );
   }
 }
