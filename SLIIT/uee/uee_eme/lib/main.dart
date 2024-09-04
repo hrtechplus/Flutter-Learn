25a484133485d0a29bed6acf40022720885aee78
HasinduOnline <40927446+hrtechplus@users.noreply.github.com>
2024-09-05 01:09:58 +0530

    chore: Refactor main.dart, add email/password form widget, and update dependencies

--- SLIIT/uee/uee_eme/lib/main.dart
+++ SLIIT/uee/uee_eme/lib/main.dart
@@ -53,50 +53,104 @@ class _MainAppState extends State<MainApp> {
 class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text("Home Screen"),
+    return Scaffold(
+      body: Center(
+        child: Text(
+          "Home Screen",
+          style: TextStyle(
+              fontSize: 24.0, fontWeight: FontWeight.bold), // Explicit styling
+        ),
+      ),
     );
   }
 }
 
 class UserProfileScreen extends StatelessWidget {
+  final _formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
-      appBar: AppBar(
-        title: Text("User Profile"),
-      ),
       body: SafeArea(
-        child: SingleChildScrollView(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            children: [
-              TextFormField(
-                decoration: InputDecoration(labelText: 'Full Name'),
-              ),
-              TextFormField(
-                decoration: InputDecoration(labelText: 'Blood Group'),
-              ),
-              TextFormField(
-                decoration: InputDecoration(labelText: 'Age'),
-              ),
-              TextFormField(
-                decoration: InputDecoration(labelText: 'Additional Note'),
-              ),
-              TextFormField(
-                decoration:
-                    InputDecoration(labelText: 'Emergency Contact Number'),
-              ),
-              ElevatedButton(
-                onPressed: () {
-                  // Implement save functionality
-                },
-                child: Text('Save Profile'),
-              ),
-            ],
+        child: LayoutBuilder(builder: (context, constraints) {
+          return SingleChildScrollView(
+            padding: const EdgeInsets.symmetric(horizontal: 16.0),
+            child: Column(
+              children: [
+                SizedBox(height: constraints.maxHeight * 0.08),
+                Image.network(
+                  "https://i.postimg.cc/nz0YBQcH/Logo-light.png",
+                  height: 100,
+                ),
+                SizedBox(height: constraints.maxHeight * 0.08),
+                Text(
+                  "Medical Profile",
+                  style: Theme.of(context)
+                      .textTheme
+                      .headlineSmall!
+                      .copyWith(fontWeight: FontWeight.bold),
+                ),
+                SizedBox(height: constraints.maxHeight * 0.05),
+                Form(
+                  key: _formKey,
+                  child: Column(
+                    children: [
+                      buildTextFormField(context, 'Full Name'),
+                      buildTextFormField(context, 'Blood Group'),
+                      buildTextFormField(context, 'Age'),
+                      buildTextFormField(context, 'Additional Note'),
+                      buildTextFormField(context, 'Emergency Contact'),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(vertical: 16.0),
+                        child: ElevatedButton(
+                          onPressed: () {
+                            if (_formKey.currentState!.validate()) {
+                              _formKey.currentState!.save();
+                              // Process data here or navigate to another screen
+                            }
+                          },
+                          style: ElevatedButton.styleFrom(
+                            elevation: 0,
+                            backgroundColor: const Color(0xFF00BF6D),
+                            foregroundColor: Colors.white,
+                            minimumSize: const Size(double.infinity, 48),
+                            shape: const StadiumBorder(),
+                          ),
+                          child: const Text("Save Profile"),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          );
+        }),
+      ),
+    );
+  }
+
+  Widget buildTextFormField(BuildContext context, String hint) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 8.0),
+      child: TextFormField(
+        decoration: InputDecoration(
+          hintText: hint,
+          filled: true,
+          fillColor: Color(0xFFF5FCF9),
+          contentPadding:
+              EdgeInsets.symmetric(horizontal: 16.0 * 1.5, vertical: 16.0),
+          border: OutlineInputBorder(
+            borderSide: BorderSide.none,
+            borderRadius: BorderRadius.all(Radius.circular(50)),
           ),
         ),
+        validator: (value) {
+          if (value == null || value.isEmpty) {
+            return 'Please enter $hint';
+          }
+          return null;
+        },
       ),
     );
   }
