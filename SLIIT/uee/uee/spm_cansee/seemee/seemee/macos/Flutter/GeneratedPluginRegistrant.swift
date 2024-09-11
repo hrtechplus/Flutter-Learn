ccf0bb1049e8556876a54a5e2c415d0c2e9c0b3b
HasinduOnline <40927446+hrtechplus@users.noreply.github.com>
2024-09-11 15:25:25 +0530

    Refactor GeneratedPluginRegistrant.swift to add speech-to-text functionality- no errors

--- SLIIT/uee/uee/spm_cansee/seemee/seemee/macos/Flutter/GeneratedPluginRegistrant.swift
+++ SLIIT/uee/uee/spm_cansee/seemee/seemee/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -9,10 +9,12 @@ import cloud_firestore
 import firebase_core
 import firebase_database
 import flutter_tts
+import speech_to_text
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseFirestorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseFirestorePlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   FLTFirebaseDatabasePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseDatabasePlugin"))
   FlutterTtsPlugin.register(with: registry.registrar(forPlugin: "FlutterTtsPlugin"))
+  SpeechToTextPlugin.register(with: registry.registrar(forPlugin: "SpeechToTextPlugin"))
 }
