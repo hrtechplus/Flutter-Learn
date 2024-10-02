721d49c23dd575796079a2a911c809f30e9128ae
HasinduOnline <40927446+hrtechplus@users.noreply.github.com>
2024-10-03 00:00:41 +0530

    Refactor delete_feedback_screen.dart to handle swipe gestures for navigation

--- SLIIT/uee/uee/spm_cansee/seemee/seemee/lib/screens/delete_feedback_screen.dart
+++ SLIIT/uee/uee/spm_cansee/seemee/seemee/lib/screens/delete_feedback_screen.dart
@@ -102,12 +102,14 @@ class _DeleteFeedbackScreenState extends State<DeleteFeedbackScreen> {
   // Handle swipe gestures to go back to HomeScreen
   void _handleSwipe(DragEndDetails details) {
     if (details.primaryVelocity! > 0) {
+      _flutterTts.speak("Back to HomeScreen");
       // Swipe right (go back)
       Navigator.pushReplacement(
         context,
         MaterialPageRoute(builder: (context) => const HomeScreen()),
       );
     } else if (details.primaryVelocity! < 0) {
+      _flutterTts.speak("Back to HomeScreen");
       // Swipe left (go back)
       Navigator.pushReplacement(
         context,
